ba515eb6b9a178a9397a3bd3a0ed1dc716e27ea8
Lukasz <lukaszciesluk@gmail.com>
2020-06-26 18:48:40 +0100

    Dun Laoghaire in Dublin (resize images)

--- _site/sw.js
+++ _site/sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202006261834",
+        "/css/pixyll.css?202006261847",
         "/"
       ]);
     })
